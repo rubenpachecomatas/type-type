14599a658a7ba471005d9e2afa300db9a3d0b76f
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2024-01-20 13:23:49 +0100

    Add word input logic and words styles

--- app/_components/typer/Typer.tsx
+++ app/_components/typer/Typer.tsx
@@ -13,9 +13,10 @@ import { Input } from "@/components/ui/input";
 import { RefreshCcw } from "lucide-react";
 import WordsSelect from "./_components/WordsSelect";
 import useTyper from "./hooks/use-typer";
+import { cn } from "@/lib/utils";
 
 const Typer = () => {
-  const { wordsList } = useTyper();
+  const { currentWord, onChange, value, verifyWord, wordsList } = useTyper();
 
   return (
     <div className=" max-w-2xl py-10 flex flex-col gap-5 items-start">
@@ -28,12 +29,26 @@ const Typer = () => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          {wordsList.map(({ word }, i) => (
-            <span key={i}>{word} </span>
+          {wordsList.map(({ word, isCorrect }, i) => (
+            <span
+              className={cn(
+                currentWord === i && " text-violet-500",
+                isCorrect !== null &&
+                  (isCorrect ? "text-green-500" : "text-red-500")
+              )}
+              key={i}
+            >
+              {word}{" "}
+            </span>
           ))}
         </CardContent>
         <CardFooter className="flex justify-between gap-5">
-          <Input placeholder="Type here" />
+          <Input
+            value={value}
+            onChange={onChange}
+            placeholder="Type here"
+            onKeyDown={verifyWord}
+          />
           <Button>
             <RefreshCcw className="size-4" />
           </Button>
--- app/_components/typer/hooks/use-typer.ts
+++ app/_components/typer/hooks/use-typer.ts
@@ -1,13 +1,39 @@
 import { useTyperStore } from "@/store/use-typer";
-import { useEffect, useState } from "react";
+import {
+  FormEvent,
+  KeyboardEvent,
+  SyntheticEvent,
+  useEffect,
+  useState,
+} from "react";
 import { getMultipleRandom } from "../utils/Typer.utils";
 import { WORDS_OPTIONS } from "../constants/Typer.constants";
 import { Word } from "./types/word.type";
 
 const useTyper = () => {
+  const [value, setValue] = useState<string>("");
   const [wordsList, setWordsList] = useState<Word[]>([]);
+  const [currentWord, setCurrentWord] = useState<number>(0);
 
   const { words } = useTyperStore((state) => state);
+
+  const onChange = (e: FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.value === " ") return;
+    setValue(e.currentTarget.value);
+  };
+
+  const verifyWord = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== " ") return;
+    setWordsList((prev) =>
+      prev.map((word, i) => {
+        if (i === currentWord)
+          return { ...word, isCorrect: value === word.word };
+        else return word;
+      })
+    );
+    setCurrentWord((prev) => prev + 1);
+    setValue("");
+  };
 
   useEffect(() => {
     setWordsList(
@@ -19,6 +45,10 @@ const useTyper = () => {
   }, [words]);
 
   return {
+    currentWord,
+    value,
+    onChange,
+    verifyWord,
     wordsList,
   };
 };
