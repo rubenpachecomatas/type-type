7dc0075aa6efc5f4b0d8b8dd6970b655d0bd3cd0
Rubén Pacheco Matas <ruben.pacheco.matas@gmail.com>
2024-01-20 21:08:05 +0100

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
       <body className={onest.className}>{children}</body>
     </html>
   );
